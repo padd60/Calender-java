1db9616d0f9f93c281cbfa73c778761302e9008f
junghwankim <padd60@naver.com>
2021-07-06 21:23:05 +0900

    Updating Leap year

--- src/hello/calender/Prompt.java
+++ src/hello/calender/Prompt.java
@@ -12,18 +12,23 @@ public class Prompt {
 			Calender cal = new Calender();
 			
 			int month = 1;
+			int year = 2021;
 			
 			while (true) {
+				System.out.println("년도를 입력해주세요.");
+				System.out.println("YEAR> ");
+				year = scanner.nextInt();
 				System.out.println("월을 입력해주세요.");
-				System.out.println(PROMPT);
+				System.out.println("MONTH> ");
 				month = scanner.nextInt();
+				
 				if (month <= -1) {
 					break;
 				} else if (month > 12) {
 					continue;
 				}
 				
-				cal.printCalendar(2021, month);
+				cal.printCalendar(year, month);
 			}
 	
 			System.out.println("끝났습니다.");
