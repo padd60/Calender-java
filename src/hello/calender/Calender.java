7d40bd552bebd7168fe04f4a77e70c5455167779
junghwankim <padd60@naver.com>
2021-07-06 22:48:47 +0900

    Update calender weekday and line change fuction

--- src/hello/calender/Calender.java
+++ src/hello/calender/Calender.java
@@ -1,42 +1,56 @@
 package hello.calender;
 
-
-
 public class Calender {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public boolean isLeapYear(int year) {
-		if(year %4 ==0 && (year % 100 != 0 || year % 400 == 0)) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
 		} else {
-		return false;
-		}
-	}
-	
-	public int maxDaysOfMonth(int year,int month) {
-		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
-		}else {
-		return MAX_DAYS[month - 1];
+			return false;
 		}
 	}
 
-	public void printCalendar(int year, int month) {	
+	public int maxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
+		// 첫줄 빈칸 출력
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
 		int maxDay = maxDaysOfMonth(year, month);
-		
-		for(int i = 1; i <= maxDay; i++) {
-			System.out.printf("%3d",i);
-			if (i % 7 == 0) {
-				System.out.println();				
+		int count = 7 - weekday;
+		int newcount = (count < 7) ? count : 0;
+
+		// 첫줄 출력
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		// 두번째 줄 출력
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == newcount) {
+				System.out.println();
 			}
 		}
-		
+
 		System.out.println();
-	}	
+		System.out.println();
+	}
 }
--- src/hello/calender/Prompt.java
+++ src/hello/calender/Prompt.java
@@ -3,37 +3,67 @@ package hello.calender;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	private final static String PROMPT= "cal>";
-	
+
+	// week 는 요일
+	// 요일은 일요일부터 순서대로 0 ~ 6
+
+	public int parseDay(String week) {
+		if (week == "SU") {
+			return 0;
+		} else if (week.equals("MO")) {
+			return 1;
+		} else if (week.equals("TU")) {
+			return 2;
+		} else if (week.equals("WE")) {
+			return 3;
+		} else if (week.equals("TH")) {
+			return 4;
+		} else if (week.equals("FR")) {
+			return 5;
+		} else if (week.equals("SA")) {
+			return 6;
+		} else {
+			return 0;
+		}
+	}
+
 	public void runPrompt() {
 
-			Scanner scanner = new Scanner(System.in);
-			Calender cal = new Calender();
-			
-			int month = 1;
-			int year = 2021;
-			
-			while (true) {
-				System.out.println("년도를 입력해주세요.");
-				System.out.println("YEAR> ");
-				year = scanner.nextInt();
-				System.out.println("월을 입력해주세요.");
-				System.out.println("MONTH> ");
-				month = scanner.nextInt();
-				
-				if (month <= -1) {
-					break;
-				} else if (month > 12) {
-					continue;
-				}
-				
-				cal.printCalendar(year, month);
+		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+
+		int month = 1;
+		int year = 2021;
+		int weekday = 0;
+
+		while (true) {
+			System.out.println("년도를 입력해주세요.(종료: -1)");
+			System.out.println("YEAR> ");
+			year = scanner.nextInt();
+
+			if (year == -1) {
+				break;
 			}
-	
-			System.out.println("끝났습니다.");
-			scanner.close();
+			System.out.println("월을 입력해주세요.");
+			System.out.println("MONTH> ");
+			month = scanner.nextInt();
+			
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			
+			System.out.println("첫째 날의 요일을 입력해주세요(SU, MO, TU, WE, TH, FR, SA).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+
+			cal.printCalendar(year, month, weekday);
 		}
+
+		System.out.println("끝났습니다.");
+		scanner.close();
+	}
 
 	public static void main(String[] args) {
 		// 셀 실행
